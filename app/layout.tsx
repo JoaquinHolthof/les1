0b2afab9ac1fcad4a1b22b47a2ab92e21fda40d6
Joaquin <joaquin.holthof@gmail.com>
2026-03-02 11:37:18 +0100

    Google Tag Manager toegevoegd aan layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* 1. Google Tag Manager Script */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+          `}
+        </Script>
+
+        {/* Je oude GA4 scripts (optioneel als je GTM gebruikt) */}
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
           strategy="afterInteractive"
@@ -35,9 +47,17 @@ export default function RootLayout({
       </head>
 
       <body>
-        {/* Google Analytics page view tracker */}
-        <GoogleAnalyticsTracker />
+        {/* 2. GTM Noscript (verplicht voor SEO en gebruikers zonder JS) */}
+        <noscript>
+          <iframe 
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
 
+        <GoogleAnalyticsTracker />
         <GlobalNavbar />
 
         <div className="p-20 mx-auto mt-10">
